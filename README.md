94002a6bb9319db785ca430ad2209c25f9998984
shimomo <developer@shimomo.net>
2023-06-05 02:56:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Boatrace Ninja Trimmer
+
+[![Latest Stable Version](https://poser.pugx.org/boatrace-ninja/trimmer/v/stable)](https://packagist.org/packages/boatrace-ninja/trimmer)
+[![Latest Unstable Version](https://poser.pugx.org/boatrace-ninja/trimmer/v/unstable)](https://packagist.org/packages/boatrace-ninja/trimmer)
+[![License](https://poser.pugx.org/boatrace-ninja/trimmer/license)](https://packagist.org/packages/boatrace-ninja/trimmer)
+
+## Installation
+```
+$ composer require boatrace-ninja/trimmer
+```
+
+## Usage
+```php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Boatrace\Ninja\Trimmer;
+
+var_dump(Trimmer::trim()); // NULL
+var_dump(Trimmer::trim(null)); // NULL
+var_dump(Trimmer::trim(' 競艇 ')); // string(6) "競艇"
+
+var_dump(Trimmer::ltrim()); // NULL
+var_dump(Trimmer::ltrim(null)); // NULL
+var_dump(Trimmer::ltrim(' 競艇 ')); // string(7) "競艇 "
+
+var_dump(Trimmer::rtrim()); // NULL
+var_dump(Trimmer::rtrim(null)); // NULL
+var_dump(Trimmer::rtrim(' 競艇 ')); // string(7) " 競艇"
+```
+
+## License
+The Boatrace Ninja Trimmer is open source software licensed under the [MIT license](LICENSE).
---
+++ tests/MainTrimmerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Boatrace\Ninja\Tests;
+
+use Boatrace\Ninja\MainTrimmer;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/**
+ * @author shimomo
+ */
+class MainTrimmerTest extends PHPUnitTestCase
+{
+    /**
+     * @var \Boatrace\Ninja\MainTrimmer
+     */
+    protected $trimmer;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->trimmer = new MainTrimmer;
+    }
+
+    /**
+     * @return void
+     */
+    public function testTrim(): void
+    {
+        $this->assertNull($this->trimmer->trim());
+        $this->assertNull($this->trimmer->trim(null));
+        $this->assertSame('競艇', $this->trimmer->trim(' 競艇 '));
+    }
+
+    /**
+     * @return void
+     */
+    public function testLtrim(): void
+    {
+        $this->assertNull($this->trimmer->ltrim());
+        $this->assertNull($this->trimmer->ltrim(null));
+        $this->assertSame('競艇 ', $this->trimmer->ltrim(' 競艇 '));
+    }
+
+    /**
+     * @return void
+     */
+    public function testRtrim(): void
+    {
+        $this->assertNull($this->trimmer->rtrim());
+        $this->assertNull($this->trimmer->rtrim(null));
+        $this->assertSame(' 競艇', $this->trimmer->rtrim(' 競艇 '));
+    }
+}
